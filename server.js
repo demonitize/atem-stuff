3bb02246fbf6c8d54df098989ec0122182fc44f1
Demonitized <admin@demonitize.is-a.dev>
2023-02-05 11:48:24 -0800

    moved to switch statement

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-const {Atem} = require("atem-connection"); // Library for ATEM
+const { Atem } = require("atem-connection"); // Library for ATEM
 const colors = require('colors/safe'); // Library for console colors
+const config = require("./config.json"); // config import
 var cache;
-const myAtem = new Atem(); // Init ATEM
-myAtem.on('info', console.log); // Register ATEM events
+const myAtem = new Atem({debugBuffers:config.connection.debug}); // Init ATEM
+if (config.connection.debug) {
+    myAtem.on('info', console.log); // Register ATEM events
+    myAtem.on('debug', console.log); // Register ATEM events
+}
 myAtem.on('error', console.error); // Register ATEM events
 
-myAtem.connect('192.168.100.89'); // Define the ATEM IP address
+myAtem.connect(config.connection.ip); // Define the ATEM IP address
 
 myAtem.on('stateChanged', (state, pathToChange) => {
     if (state.streaming.stats.cacheUsed == cache) return;
-	cache = state.streaming.stats.cacheUsed; // The cache level 
-    if (cache == 100) {
-        console.error(colors.bgRed(` ALERT! CACHE IS FULL! Please check the network connection, and restart stream. Current cache usage: ${cache}% `));
-    } else if (cache >= 95) {
-        console.warn(colors.bgRed(` WARNING! Cache level is above 95%. Current cache usage: ${cache}% `));
-    } else if (cache >= 85) {
-        console.warn(colors.bgYellow(` WARNING! Cache level is above 85%. Current cache usage: ${cache}% `));
-    } else if (cache >= 75) {
-        console.warn(colors.bgYellow(` ALERT! Cache level is above 75%. Current cache usage: ${cache}% `));
-    } else if (cache >= 50) {
-        console.warn(colors.bgGreen(` ALERT! Cache level is above 50%. Current cache usage: ${cache}% `));
-    } else {
-        console.log(colors.bgCyan(` INFO: Cache level at ${cache}% `));
+    cache = state.streaming.stats.cacheUsed; // The cache level 
+    switch (cache) {
+        case this == 100:
+            console.error(colors.bgRed(` ALERT! CACHE IS FULL! Please check the network connection, and restart stream. Current cache usage: ${cache}% `));
+            break;
+        case this >= 95:
+            console.warn(colors.bgRed(` WARNING! Cache level is above 95%. Current cache usage: ${cache}% `));
+            break;
+        case this >= 85:
+            console.warn(colors.bgYellow(` WARNING! Cache level is above 85%. Current cache usage: ${cache}% `));
+            break;
+        case this >= 75:
+            console.warn(colors.bgYellow(` WARNING! Cache level is above 75%. Current cache usage: ${cache}% `));
+            break;
+        case this >= 50:
+            console.warn(colors.bgGreen(` INFO! Cache level is above 50%. Current cache usage: ${cache}% `));
+            break;
+        default:
+            console.log(colors.bgCyan(` INFO: Cache level at ${cache}% `));
+            break;
+
     }
 
 });
